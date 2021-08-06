12aa8d79fefa12d29d36f230d8f10c7b9fc33c29
eduardo3g <eduardosbrasil10@gmail.com>
2021-08-06 19:02:54 -0300

    feat: implement handlers to return usage reports

--- handler.js
+++ handler.js
@@ -1,15 +1,45 @@
-'use strict';
+"use strict";
+const AWS = require("aws-sdk");
+const apiGateway = new AWS.APIGateway();
+const moment = require("moment");
 
-module.exports.hello = async (event) => {
+const hello = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v2.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: "Hello world",
   };
+};
+
+const usagePlans = async (event) => {
+  const result = await apiGateway.getUsagePlans().promise();
+  console.log("Usage plans", result);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result, null, 2),
+  };
+};
+
+const usage = async (event) => {
+  const { from, to, usagePlanId, keyId } = event.queryStringParameters;
+
+  const usage = await apiGateway
+    .getUsage({
+      startDate: moment(from).format("YYY-MM-DD"),
+      endDate: moment(to).format("YYY-MM-DD"),
+      usagePlanId,
+      keyId,
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(usage, null, 2),
+  };
+};
+
+module.exports = {
+  hello,
+  usagePlans,
+  usage,
 };
